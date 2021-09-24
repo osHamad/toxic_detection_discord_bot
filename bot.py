b966b186be0e1b8d7b8f009b9c70efd5afc181ea
Osama Hamad <osamahama2005@gmail.com>
2021-09-23 20:14:26 -0400

    change token to external file

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import pickle
 import discord
 from discord.ext import commands
 
+with open('token.txt', 'r') as token_file:
+    token = (token_file.read())
+
 loaded_model = pickle.load(open('xgb_model.sav', 'rb'))
 loaded_vectorizer = pickle.load(open('vectorizer.sav', 'rb'))
 
