38c52ca7fe25bb78e28c46bb255c3d43efdf19a6
Osama Hamad <osamahama2005@gmail.com>
2021-09-25 17:02:55 -0400

    add new feature- reply to toxic message with mod ping

--- bot.py
+++ bot.py
@@ -8,7 +8,22 @@ with open('token.txt', 'r') as token_file:
 loaded_model = pickle.load(open('xgb_model.sav', 'rb'))
 loaded_vectorizer = pickle.load(open('vectorizer.sav', 'rb'))
 
+
 client = commands.Bot(command_prefix='.')
+
+# instant ban/mute
+# democracy
+# notify mods
+def notify_mods(message):
+    message.reply('hello world')
+
+# 3 strike system
+
+
+@client.command(aliases=['id'])
+async def get_bot_id(ctx):
+    print('activated')
+    await ctx.send(client.user.id)
 
 
 @client.event
@@ -21,7 +36,13 @@ async def on_message(message):
     msg = [message.content]
     vectorized = loaded_vectorizer.transform(msg)
     prediction = loaded_model.predict(vectorized)
-    if prediction == [1] and message.author.id != 772983440802578442:
-        await message.channel.send('Toxic comment detected.')
+    if prediction == [1] and message.author.id != client.user.id:
+        await message.reply('<@772983440802578442>')
+
+    await client.process_commands(message)
+
+
+
+
 
 client.run(token)
