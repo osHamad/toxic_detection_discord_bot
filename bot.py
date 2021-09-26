a80f046845323e0781556bfa57670e51c78fe9ad
Osama Hamad <osamahama2005@gmail.com>
2021-09-26 10:32:56 -0400

    add bot reaction for commands

--- bot.py
+++ bot.py
@@ -14,8 +14,7 @@ client = commands.Bot(command_prefix='.')
 # instant ban/mute
 # democracy
 # notify mods
-def notify_mods(message):
-    message.reply('hello world')
+
 
 # 3 strike system
 
@@ -37,15 +36,23 @@ async def on_message(message):
     vectorized = loaded_vectorizer.transform(msg)
     prediction = loaded_model.predict(vectorized)
     if prediction == [1] and message.author.id != client.user.id:
-        client_msg = await message.reply('<@' + str(message.guild.owner_id) + '>, It appears that toxic behaviour took '
-                                                                              'place, do you want to take action?')
-        await client_msg.add_reaction('\u2705')
-        await client_msg.add_reaction('\u274c')
+        if True:
+            client_msg = await message.reply('<@' + str(message.guild.owner_id) + '>, It appears that toxic behaviour took '
+                                                                            'place, do you want to take action?')
+            await client_msg.add_reaction('\u2705')
+            await client_msg.add_reaction('\u274c')
+
 
     await client.process_commands(message)
 
 
-
+@client.event
+async def on_reaction_add(reaction, user):
+    if user != client.user:
+        if str(reaction.emoji) == "\u2705":
+            print('stay')
+        if str(reaction.emoji) == "\u274c":
+            print('delete')
 
 
 client.run(token)
